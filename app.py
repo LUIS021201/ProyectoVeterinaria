1ea7fbe493d0a7c1212dcb2df5c0813dc897721c
LUIS021201 <luisernestohernandez1@hotmail.com>
2022-04-08 22:02:34 -0700

    Agregar lista_usuarios.html, usuarios.py.Agregar menu de acceso index.html de acuerdo a usuario. Corregir estilos de login.html

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask, redirect, render_template, request, session
+from usuarios import get_dicc_usuarios, get_dicc_accesos, get_lista_usuarios
 
 app = Flask(__name__)
-app.secret_key ='lwiu74dhn2SuF3j'
-diccionario_usuarios = {'LuisHL':{'password':'123','nombre':'Luis Hernandez'}}
+app.secret_key = 'lwiu74dhn2SuF3j'
+diccionario_usuarios = get_dicc_usuarios()
+
+diccionario_accesos = get_dicc_accesos()
+
+
+# @app.context_processor
+# def handle_context():
+
 
 @app.route("/")
 def index():
-    if 'logged_in' in session:
+    if 'logged_in' in session.keys():
         if session['logged_in']:
-            return render_template("index.html")
-        else:
-            return redirect("/login")
-    else:
-        return redirect("/login")
+            accesos = diccionario_accesos[session['type']]
+            usuario = diccionario_usuarios[session['username']]
 
-@app.route("/login", methods=['GET','POST'])
+            return render_template("index.html", accesos=accesos, log=['Log Out', '/logout'], usuario=usuario)
+        else:
+            return render_template("index.html", log=['Log In', '/login'])
+    else:
+        return render_template("index.html", log=['Log In', '/login'])
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template("login.html")
@@ -26,10 +38,12 @@ def login():
                 session['username'] = username
                 session['nombre'] = diccionario_usuarios[username]['nombre']
                 session['logged_in'] = True
+                session['type'] = diccionario_usuarios[username]['type']
                 return redirect("/")
             else:
                 mensaje = 'Usuario o contraseña incorrectos'
-                return render_template("login.html",mensaje=mensaje)
+                return render_template("login.html", mensaje=mensaje)
+
 
 @app.route("/logout", methods=['GET'])
 def logout():
@@ -37,8 +51,25 @@ def logout():
     return redirect("/")
 
 
+@app.route("/usuarios", methods=['GET', 'POST'])
+def usuarios():
+    if 'logged_in' in session.keys():
+        if session['logged_in']:
+            accesos = diccionario_accesos[session['type']]
+            usuario = diccionario_usuarios[session['username']]
+            lista_usuarios = get_lista_usuarios()
+            return render_template("lista_usuarios.html", accesos=accesos, log=['Log Out', '/logout'], usuario=usuario,
+                                    lista_usuarios=lista_usuarios)
+        else:
+            return render_template("index.html", log=['Log In', '/login'])
+    else:
+        return render_template("index.html", log=['Log In', '/login'])
+    # accesos = diccionario_accesos[session['type']]
+    # usuario = diccionario_usuarios[session['username']]
+    # lista_usuarios = get_lista_usuarios()
+    # return render_template("lista_usuarios.html", accesos=accesos, log=['Log Out', '/logout'], usuario=usuario,
+    #                        lista_usuarios=lista_usuarios)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
---
+++ usuarios.py
@@ -0,0 +1,54 @@
+diccionario_usuarios = {'LuisHL': {'password': '123', 'nombre': 'Luis Hernandez', 'type': 'admin'}}
+diccionario_accesos = {'admin': {
+    '/agendarCita': 'Agendar Cita',
+    '/historial_recetas': 'Historial De Recetas',
+    '/historial_atencion': 'Historial De Atención',
+    '/agregar_receta': 'Agregar Una Receta',
+    '/agregar_atencion': 'Agregar Una Atención',
+    '/usuarios': 'Usuarios',
+    '/medicinas': 'Medicinas',
+    '/servicios': 'Servicios',
+    '/informe_ventas': 'Informe De Ventas'
+},
+    'cliente': {
+        '/agendarCita': 'Agendar Cita',
+        '/historial_recetas': 'Historial De Recetas',
+        '/historial_atencion': 'Historial De Atención'
+
+    },
+    'usuario': {
+        '/agendarCita': 'Agendar Cita',
+        '/historial_recetas': 'Historial De Recetas',
+        '/historial_atencion': 'Historial De Atención',
+        '/agregar_receta': 'Agregar Una Receta',
+        '/agregar_atencion': 'Agregar Una Atención'
+
+    }}
+
+
+# DICCIONARIO BASE CON LINK Y ACCESO
+# dicc = {
+#     '/agendarCita': 'Agendar Cita',
+#     '/historial_recetas': 'Historial De Recetas',
+#     '/historial_atencion': 'Historial De Atención',
+#     '/agregar_receta': 'Agregar Una Receta',
+#     '/agregar_atencion': 'Agregar Una Atención',
+#     '/usuarios': 'Usuarios',
+#     '/medicinas': 'Medicinas',
+#     '/servicios': 'Servicios',
+#     '/informe_ventas': 'Informe De Ventas'
+# }
+
+def get_lista_usuarios():
+    lista_usuarios = []
+    for dicc in diccionario_usuarios.values():
+        lista_usuarios.append(dicc['nombre'])
+    return lista_usuarios
+
+
+def get_dicc_usuarios():
+    return diccionario_usuarios
+
+
+def get_dicc_accesos():
+    return diccionario_accesos
