d419c0dd6d0ad083dff37fc0d1edfd61a2f62cd9
Andrea D <andreadh01@gmail.com>
2022-04-13 01:59:41 -0700

    Agendar cita

--- petvet-script.sql
+++ petvet-script.sql
@@ -1,9 +1,24 @@
 CREATE database petvet;
 use petvet;
+DROP TABLE USERS;
 CREATE TABLE users(
-	email varchar(150) NOT NULL,
-    password varchar(150) NOT NULL,
+	id int AUTO_INCREMENT NOT NULL,
+	email varchar(150) NOT NULL UNIQUE,
+    password varchar(150) NOT NULL UNIQUE,
     username varchar(150) NOT NULL,
 	name varchar(150) NOT NULL,
-	type enum('admin','usuario','cliente') NOT NULL
+	type enum('admin','usuario','cliente') NOT NULL,
+	PRIMARY KEY(id)
+) ENGINE=MyISAM default char set=latin1;
+
+drop table citas;
+CREATE TABLE citas(
+	email varchar(150) NOT NULL,
+	nombre_dueno varchar(150) NOT NULL,
+	nombre_mascota varchar(150),
+	tipo_mascota varchar(150),
+    fecha date NOT NULL,
+	hora time NOT NULL,
+    atencion enum('veterinaria','boutique') NOT NULL,
+	PRIMARY KEY(fecha, hora)
 ) ENGINE=MyISAM default char set=latin1;
