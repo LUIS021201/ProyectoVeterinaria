986cced5ce4c98d16ff29b55ec52b7dbbace29db
LUIS021201 <luisernestohernandez1@hotmail.com>
2022-04-21 19:38:26 -0700

    Agregar agregar_medicina.html. Agregar metodo "agregar_medicina()" en app.py.

--- recetas.py
+++ recetas.py
@@ -5,19 +5,33 @@ def insertar_medicina(nombre, descripcion, presentacion, medida, stock, precio):
     conexion = obtener_conexion()
     nombre = nombre.title()
     with conexion.cursor() as cursor:
-        cursor.execute("INSERT INTO medicinas (nombre,descripcion, presentacion, medida, stock, precio) VALUES (%s, %s, %s, %s, %s,%s)",
-                       (nombre,descripcion, presentacion, medida, stock, precio))
+        cursor.execute(
+            "INSERT INTO medicinas (nombre,descripcion, presentacion, medida, stock, precio) VALUES (%s, %s, %s, %s, %s,%s)",
+            (nombre, descripcion, presentacion, medida, stock, precio))
     conexion.commit()
     conexion.close()
 
 
-def modificar_medicina(id,nombre, descripcion, presentacion, medida, stock, precio):
+def modificar_medicina(id, nombre, descripcion, presentacion, medida, stock, precio):
     conexion = obtener_conexion()
     with conexion.cursor() as cursor:
-        cursor.execute("UPDATE medicinas SET nombre = %s, descripcion = %s, presentacion = %s, medida = %s, stock = %s, precio= %s WHERE id =%s",
-                       (nombre, descripcion, presentacion, medida,stock, precio, id))
+        cursor.execute(
+            "UPDATE medicinas SET nombre = %s, descripcion = %s, presentacion = %s, medida = %s, stock = %s, precio= %s WHERE id =%s",
+            (nombre, descripcion, presentacion, medida, stock, precio, id))
     conexion.commit()
     conexion.close()
+
+
+def medicina_existe(nombre: str, descripcion: str, presentacion: str, medida: str) -> bool:
+    conexion = obtener_conexion()
+
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT * FROM medicinas WHERE nombre=%s AND descripcion=%s AND presentacion=%s AND medida=%s",(nombre,descripcion,presentacion,medida))
+        if cursor.fetchone() is None:
+            return False
+    conexion.commit()
+    conexion.close()
+    return True
 
 
 def get_lista_medicinas() -> list:
@@ -31,7 +45,13 @@ def get_lista_medicinas() -> list:
     conexion.close()
     return lista
 
-if __name__=='__main__':
-    insertar_medicina('Acepromazine','Tranquilizante/sedante para perros, gatos, caballos y otros animales.','Pastillas','mg','10','100')
-    insertar_medicina('Codeine','Usada para tratar el dolor leve a moderado en mascotas. También se puede usar como un supresor de la tos o como medicamento contra la diarrea.','Pastillas','mg','5','250')
-    insertar_medicina('Brosin','Para el tratamiento de heridas simples o infectadas, llagas, quemaduras, dermatitis pústulas y eccema.','Pomada','mg','3','125')
+
+if __name__ == '__main__':
+    insertar_medicina('Acepromazine', 'Tranquilizante/sedante para perros, gatos, caballos y otros animales.',
+                      'Pastillas', 'mg', '10', '100')
+    insertar_medicina('Codeine',
+                      'Usada para tratar el dolor leve a moderado en mascotas. También se puede usar como un supresor de la tos o como medicamento contra la diarrea.',
+                      'Pastillas', 'mg', '5', '250')
+    insertar_medicina('Brosin',
+                      'Para el tratamiento de heridas simples o infectadas, llagas, quemaduras, dermatitis pústulas y eccema.',
+                      'Pomada', 'mg', '3', '125')
